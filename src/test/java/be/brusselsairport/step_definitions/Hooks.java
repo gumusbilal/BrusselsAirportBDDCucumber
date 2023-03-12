5845552294a01caba75f2ca539b52915a800b96b
bilal gumus <gumusbll@gmail.com>
2023-03-12 21:35:16 +0100

    added framework tools

---
+++ src/test/java/be/brusselsairport/step_definitions/Hooks.java
@@ -0,0 +1,42 @@
+package be.brusselsairport.step_definitions;
+
+import be.brusselsairport.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.time.Duration;
+
+public class Hooks {
+
+    @Before
+    public void setUp(Scenario scenario){
+        Driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        Driver.get().manage().window().maximize();
+        System.out.println(scenario.getName());
+    }
+
+    @AfterStep
+    public void step(Scenario step) {
+        System.out.println("Step is " + step.getStatus());
+
+    }
+    @After
+    public void tearDown(Scenario scenario){
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+
+        System.out.println("Scenario is " + scenario.getStatus());
+        Driver.closeDriver();
+
+    }
+
+
+
+
+}
